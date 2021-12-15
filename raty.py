7ef00b4f9b6306d2b6fe1a0d164b3f6ea2b024bf
Kamil <kamilrusztyn@gmail.com>
2021-12-15 20:03:12 +0100

    dodany input do pobrania wartosci

--- raty.py
+++ raty.py
@@ -4,3 +4,6 @@ sty1, lut1, mar1, kwi1, maj1, cze1, lip1, sie1, wrz1, paz1, lis1, gru1 = (1.5928
 sty2, lut2, mar2, kwi2, maj2, cze2, lip2, sie2, wrz2, paz2, lis2, gru2 = (1.577035247,
 -0.292781443, 2.48619659, 0.267110318, 1.417952672, 1.054243267, 1.480520104,
 1.577035247,  -0.07742069, 1.165733399, -0.404186718, 1.499708521)
+wysokosc_kredyt = int(input())
+oprocentowanie = float(input())
+rata = int(input())
