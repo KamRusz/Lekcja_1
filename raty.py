0683db433c54ccec9e2dc73e6d68ef0a9633bace
Kamil <kamilrusztyn@gmail.com>
2021-12-15 20:06:54 +0100

    dodano instrukcje dla uzytkownika

--- raty.py
+++ raty.py
@@ -4,6 +4,10 @@ sty1, lut1, mar1, kwi1, maj1, cze1, lip1, sie1, wrz1, paz1, lis1, gru1 = (1.5928
 sty2, lut2, mar2, kwi2, maj2, cze2, lip2, sie2, wrz2, paz2, lis2, gru2 = (1.577035247,
 -0.292781443, 2.48619659, 0.267110318, 1.417952672, 1.054243267, 1.480520104,
 1.577035247,  -0.07742069, 1.165733399, -0.404186718, 1.499708521)
+print("Witaj w kalkulatorze kredytowym")
+print("Podaj wysokość kredytu")
 wysokosc_kredyt = int(input())
+print("teraz podaj oprocentowanie kredytu w %")
 oprocentowanie = float(input())
+print("na koniec podaj kwotę raty")
 rata = int(input())
